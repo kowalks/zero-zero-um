ebcf047cc979fa10a66eac8a05e4a99307651dec
GabrielHGobi <67982371+GabrielHGobi@users.noreply.github.com>
2021-04-03 20:34:08 -0300

    Added wall.py and changed map to create a sprite group of walls

--- data/map.py
+++ data/map.py
@@ -1,16 +1,16 @@
 import pygame
 from settings import *
 import player
-
-
+from wall import *
 
 
 class Map:
-    def __init__(self):
+    def __init__(self, screen):
         self.clock = pygame.time.Clock()
         pygame.key.set_repeat(500,50)
         self.my_player = player.Player(0, 0)
         self.all_sprites = pygame.sprite.Group()
+        self.walls = pygame.sprite.Group()
         self.all_sprites.add(self.my_player)
         
     def run(self, screen, running):

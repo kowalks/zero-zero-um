2414eb2abdbb23da5cf8491f1dea72bf0d6da50a
Yuri Gama <63912211+yuri-gama@users.noreply.github.com>
2021-04-28 19:54:05 -0300

    fight screen sketch

--- data/player.py
+++ data/player.py
@@ -3,66 +3,74 @@ from settings import *
 vec = pygame.math.Vector2
 
 class Character(pygame.sprite.Sprite):
-    def __init__(self, x, y, hp=100, COLOR=RED):
+    def __init__(self, x=0, y = 0, hp = 100, COLOR = RED):
         self.life = hp
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((TILESIZE, TILESIZE))
         self.image.fill(COLOR)
         self.rect = self.image.get_rect()
-        self.rect.x = x * TILESIZE
-        self.rect.y = y * TILESIZE
         self.x = x
         self.y = y
-        self.vx = 0
-        self.vy = 0
-
     def update(self):
-        self.x = self.rect.x // TILESIZE
-        self.y = self.rect.y // TILESIZE
+        self.rect.x = self.x*TILESIZE
+        self.rect.y = self.y*TILESIZE
+
+    def check_move(self, sinalx, sinaly, walls):
+        Collide = False
+        for brick in walls:
+            if (self.x == brick.x and self.y == brick.y):
+                Collide = True
+        if Collide == True:
+            self.x -= sinalx;
+            self.y -= sinaly;
+
+    def move(self, sinalx, sinaly, walls):
+        self.x += sinalx
+        self.y += sinaly
+        if sinalx == 1:
+           self.image = self.image_r
+        elif sinalx == -1:
+           self.image = self.original_image
+        # elif sinaly == 1:
+        #     self.image = pygame.transform.rotate(self.original_image, 270)
+        # else:
+        #     self.image = pygame.transform.rotate(self.original_image, 90)
+        self.check_move(sinalx, sinaly, walls)
+
+
 
 
 class Player(Character):
-    def __init__(self, x, y, *args, **kwargs):
-        super().__init__(x, y, *args, **kwargs)
-        self.original_image = pygame.image.load("img/player/player.png").convert_alpha()
+    def __init__(self, x,y, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_image = pygame.image.load("img/player/player.png").convert_alpha();
         self.original_image = pygame.transform.scale(self.original_image, (TILESIZE, TILESIZE))
-        self.image_r = pygame.image.load("img/player/player_right.png").convert_alpha()
+        self.image_r = pygame.image.load("img/player/player_right.png").convert_alpha();
         self.image_r = pygame.transform.scale(self.image_r, (TILESIZE, TILESIZE))
-        self.image = self.original_image
+        self.image = self.original_image;
+        self.x = x;
+        self.y = y;
 
-    def check_move(self, walls, dx, dy):
-        for brick in walls:
-            if abs(self.rect.x - brick.x*TILESIZE) < TILESIZE and abs(self.rect.y - brick.y*TILESIZE) < TILESIZE:
-                if dx != 0:
-                    self.rect.x = (brick.x - dx) * TILESIZE
-                if dy != 0:
-                    self.rect.y = (brick.y - dy) * TILESIZE
+    def update(self):
+        super().update();
 
-    def move(self, sinalx, sinaly, walls):
-        self.rect.x += sinalx * PLAYER_SPEED
-        self.rect.y += sinaly * PLAYER_SPEED
-
-        if sinalx == 1:
-            self.image = self.image_r
-        elif sinalx == -1:
-            self.image = self.original_image
-
-        self.check_move(walls, sinalx, sinaly)
 
 
 class Enemy(Character):
-    def __init__(self, all_sprites, enemy_sprites, player, x, y, *args, **kwargs):
-        super().__init__(x, y, *args, **kwargs)
+    def __init__(self, all_sprites, enemy_sprites, player, x,y, hp = 100, *args, **kwargs):
+        super().__init__(hp = hp,*args, **kwargs)
         self.groups = all_sprites, enemy_sprites
         pygame.sprite.Sprite.__init__(self, self.groups)
         self.original_image = pygame.image.load("img/enemies/zoimbie1_hold.png").convert_alpha()
         self.original_image = pygame.transform.scale(self.original_image, (TILESIZE, TILESIZE))
-        self.image = self.original_image
+        self.image = self.original_image;
+        self.x = x
+        self.y = y
         self.rot = 0
         self.player = player
 
     def update(self):
         super().update()
-        self.rot = (vec(self.player.x, self.player.y) - vec(self.x, self.y)).angle_to(vec(1, 0))
+        self.rot = (vec(self.player.x, self.player.y) - vec(self.x, self.y)).angle_to(vec(1,0))
         self.image = pygame.transform.rotate(self.original_image, self.rot)
 

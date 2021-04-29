af9188fc26155e1881534102798fb34814cf6542
GabrielHGobi <67982371+GabrielHGobi@users.noreply.github.com>
2021-04-29 14:34:03 -0300

    Trying to insert room1

--- data/room.py
+++ data/room.py
@@ -1,6 +1,7 @@
 import pygame
 from os import path
 import wall
+import pytmx
 
 class Room:
     def __init__(self, room_name):
@@ -16,3 +17,25 @@ class Room:
             for col, tile in enumerate(tiles):
                 if tile == '1':
                     wall.Wall(all_sprites, wall_sprites, x_room + col, y_room + row)
+
+class TiledRoom:
+    def __init__(self, filename):
+        folder = path.dirname(__file__)
+        tm = pytmx.load_pygame(path.join(folder, f'map\{filename}.tmx'), pixelalpha=True)
+        self.width = tm.width * tm.tilewidth
+        self.height = tm.height * tm.tileheight
+        self.tmxdata = tm
+
+    def render(self, surface):
+        ti = self.tmxdata.get_tile_image_by_gid
+        for layer in self.tmxdata.visible_layers:
+            if isinstance(layer, pytmx.TiledTileLayer):
+                for x, y, gid in layer:
+                    tile = ti(gid)
+                    if tile:
+                        surface.blit(tile, (x * self.tmxdata.tilewidth,
+                                            y *self.tmxdata.tileheight))
+
+    def make_room(self, rooms_surface):
+        self.render(rooms_surface)
+        return rooms_surface

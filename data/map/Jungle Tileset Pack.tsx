a55ef83b99ef5c6fbf9bafa6a21c82ced4ef6bad
GabrielHGobi <67982371+GabrielHGobi@users.noreply.github.com>
2021-04-29 15:44:30 -0300

    Fixed map image problem

--- data/map/Jungle Tileset Pack.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="Jungle Tileset Pack" tilewidth="64" tileheight="64" tilecount="25" columns="5">
- <image source="../data/img/map/jungle_tileset/tf_jungle_tileset.png" width="352" height="336"/>
-</tileset>

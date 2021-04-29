474f96bcd31e0f0f9da2293c753f222db06de0a9
TL1981 <80128519+TL1981@users.noreply.github.com>
2021-04-29 16:17:57 -0300

    Update itens.py

--- data/itens.py
+++ data/itens.py
@@ -1,55 +1,105 @@
 import pygame
 from settings import *
 
-class Item(pygame.sprite.Sprite, *args, **kwargs):
-    def __init__(self, x=0, y = 0, got = False, COLOR = LIGHTGREY, *args, **kwargs):
+
+class Item(pygame.sprite.Sprite):
+
+    def __init__(self, x=0, y = 0, got = False, COLOR = LIGHTGREY):
+        self.got = got
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((TILESIZE, TILESIZE))
         self.image.fill(COLOR)
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
-        self.got = got
-        
-    """Detecta se o jogador pegou o item""" 
+
     def check_got(self, player):
-      if (self.x == player.x and self.y == player.y):
+      if self.x == player.x and self.y == player.y:
         self.got = True
-     
+
     def update(self):
-        if(self.got)
-            """remover item da tela e adicionar ao jogador"""
         self.rect.x = self.x*TILESIZE
         self.rect.y = self.y*TILESIZE
-        
 
-"""Iteração com jogador: atributo getout"""
-class Key_item(Item):
-    def __init__(self, *args, **kwargs):
-        super().__init(*args, **kwargs)
-           
+
+class KeyItem(Item):
+    def __init__(self, all_sprites, item_sprites, player, x, y, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.groups = all_sprites, item_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.x = x
+        self.y = y
+        self.player = player
+
     def check_got(self, player):
-      if (self.x == player.x and self.y == player.y):
-        self.got = True
-        player.getout = True
- 
-class basic_attack_item(Item):
-    def __init__(self, *args, **kwargs):
-        super().__init(*args, **kwargs)
-        self.attack_points = 40
-  
-class advanced_attack_item(Item):
-    def __init__(self, *args, **kwargs):
-        super().__init(*args, **kwargs)
-        self.attack_points = 80
+        super().check_got(player)
 
-class basic_heal_item(Item):
-    def __init__(self, *args, **kwargs):
-        super().__init(*args, **kwargs)
-        self.heal_points = 30
+    def update(self):
+        super().update()
+
+
+class BasicAttackItem(Item):
+    def __init__(self, all_sprites, item_sprites, player, x, y, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.groups = all_sprites, item_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.x = x
+        self.y = y
+        self.attack_points = 40
+        self.player = player
+
+    def check_got(self, player):
+        super().check_got(player)
+
+    def update(self):
+        super().update()
+
   
-class advanced_heal_item(Item):
-    def __init__(self, *args, **kwargs):
-        super().__init(*args, **kwargs)
+class AdvancedAttackItem(Item):
+    def __init__(self, all_sprites, item_sprites, player, x, y, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.groups = all_sprites, item_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.x = x
+        self.y = y
+        self.attack_points = 80
+        self.player = player
+
+    def check_got(self, player):
+        super().check_got(player)
+
+    def update(self):
+        super().update()
+
+class BasicHealItem(Item):
+    def __init__(self, all_sprites, item_sprites, player, x, y, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.groups = all_sprites, item_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.x = x
+        self.y = y
+        self.heal_points = 30
+        self.player = player
+
+    def check_got(self, player):
+        super().check_got(player)
+
+    def update(self):
+        super().update()
+
+
+class AdvancedHealItem(Item):
+    def __init__(self, all_sprites, item_sprites, player, x, y, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.groups = all_sprites, item_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.x = x
+        self.y = y
         self.heal_points = 60
- 
+        self.player = player
+
+    def check_got(self, player):
+        super().check_got(player)
+
+    def update(self):
+        super().update()

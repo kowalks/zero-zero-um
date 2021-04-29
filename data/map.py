c967933d8ff1caaa2b3356178a78579f1ac0092c
TL1981 <80128519+TL1981@users.noreply.github.com>
2021-04-29 16:18:48 -0300

    Update map.py

--- data/map.py
+++ data/map.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
 import player
+import itens
 from wall import *
 from room import *
 
@@ -15,6 +16,20 @@ class Map:
         self.all_sprites.add(self.my_player)
         self.set_rooms()
         self.camera = Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.enemies = pygame.sprite.Group()
+        self.itens = pygame.sprite.Group()
+        self.my_enemy = player.Enemy(self.all_sprites, self.enemies, self.my_player, 1, 1)
+        self.my_enemy = player.Enemy(self.all_sprites, self.enemies, self.my_player, 5, 6)
+        self.my_enemy = player.Enemy(self.all_sprites, self.enemies, self.my_player, 7, 7)
+        self.my_enemy = player.Enemy(self.all_sprites, self.enemies, self.my_player, 7, 8)
+        self.my_enemy = player.Enemy(self.all_sprites, self.enemies, self.my_player, 10, 7)
+        self.my_enemy = player.Enemy(self.all_sprites, self.enemies, self.my_player, 7, 11)
+        self.my_item = itens.KeyItem(self.all_sprites, self.itens, self.my_player, 2, 2)
+        self.my_item = itens.BasicAttackItem(self.all_sprites, self.itens, self.my_player, 3, 3)
+        self.my_item = itens.AdvancedAttackItem(self.all_sprites, self.itens, self.my_player, 4, 4)
+        self.my_item = itens.BasicHealItem(self.all_sprites, self.itens, self.my_player, 5, 5)
+        self.my_item = itens.BasicHealItem(self.all_sprites, self.itens, self.my_player, 6, 6)
+
 
     def set_rooms(self):
         room_list = [[Room("up_left_corner"),Room("up_middle_corner"),Room("up_middle_corner"),Room("up_middle_corner"),Room("up_right_corner")],
@@ -38,7 +53,7 @@ class Map:
 
     def draw(self, screen):
         screen.fill(BGCOLOR)
-        self.draw_grid(screen)
+        # self.draw_grid(screen)
         for sprite in self.all_sprites:
             screen.blit(sprite.image, self.camera.apply(sprite))
         pygame.display.flip()
@@ -55,17 +70,19 @@ class Map:
                 self.done = True
                 running = False
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                     self.my_player.move(1,0, self.walls)
-                elif event.key == pygame.K_LEFT:
+                elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
                     self.my_player.move(-1,0, self.walls)
-                elif event.key == pygame.K_UP:
+                elif event.key == pygame.K_UP or event.key == pygame.K_w:
                     self.my_player.move(0,-1, self.walls)
-                elif event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
                     self.my_player.move(0,1, self.walls)
 
         self.all_sprites.update()
         self.camera.update(self.my_player)
+        # pygame.sprite.spritecollide(self.my_player, self.enemies, 1)
+        # pygame.sprite.spritecollide(self.my_player, self.itens, 1)
         return running
 
 

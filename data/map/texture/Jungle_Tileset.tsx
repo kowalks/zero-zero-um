67fe0c7048241fd7e6a156786d1ed24f666a9dd9
GabrielHGobi <67982371+GabrielHGobi@users.noreply.github.com>
2021-04-29 15:44:30 -0300

    Fixed map image problem

--- data/map/texture/Jungle_Tileset.tsx
+++ data/map/texture/Jungle_Tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.6.0" name="Jungle Tileset" tilewidth="16" tileheight="16" tilecount="462" columns="22">
- <image source="../img/map/jungle_tileset/tf_jungle_tileset.png" width="352" height="336"/>
+ <image source="tf_jungle_tileset.png" width="352" height="336"/>
 </tileset>

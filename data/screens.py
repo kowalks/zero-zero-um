04687ac941c35e7b1d0bcd69de326577a240288e
kowalks <guilhermekowalczuk@gmail.com>
2021-05-03 21:24:08 -0300

    beautify 2

--- data/screens.py
+++ data/screens.py
@@ -1,24 +1,25 @@
-import pygame
 import settings
 from settings import *
 import buttons
 import map
 import random as rnd
 from pygame import mixer
-class Screen():
+
+
+class Screen:
     """ Class of a generic screen in the game """
-    def __init__(self, caption = "My game name", *args, **kwargs):
+
+    def __init__(self, caption="My game name", *args, **kwargs):
         # Initialize screen
-        self.scn = pygame.display.set_mode((settings.SCREEN_WIDTH,
-                                               settings.SCREEN_HEIGHT))
+        self.scn = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
         # Load default background
-        self.bg = pygame.transform.scale( # TODO: remove this when all bg defined
+        self.bg = pygame.transform.scale(  # TODO: remove this when all bg defined
             pygame.image.load("img/background/capa.png"),
             (settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
 
         # Load caption of the screen (game name by default)
         pygame.display.set_caption(caption)
-        
+
     def set_bg(self, bg_str):
         self.bg = pygame.transform.scale(
             pygame.image.load(f'img/background/{bg_str}'),
@@ -33,10 +34,11 @@ class Screen():
             pygame.display.update()
 
     def run_events(self, running):
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-            return running
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        return running
+
 
 class TitleScreen(Screen):
 
@@ -47,21 +49,17 @@ class TitleScreen(Screen):
 
     def run_events(self, running):
 
-
         mx, my = pygame.mouse.get_pos()
 
-        ng_button = buttons.Button(settings.MARGIN,
-                                   settings.SCREEN_HEIGHT-settings.MARGIN,
-                                   "Novo jogo", "blue_button")
-        controls_button = buttons.Button(settings.MARGIN + settings.BT_DIST,
-                                   settings.SCREEN_HEIGHT - settings.MARGIN,
-                                   "Controles", "blue_button")
-        settings_button = buttons.Button(settings.MARGIN + 2*settings.BT_DIST,
-                                   settings.SCREEN_HEIGHT - settings.MARGIN,
-                                   "Opções", "blue_button")
-        quit_button = buttons.Button(settings.MARGIN + 3*settings.BT_DIST,
-                                   settings.SCREEN_HEIGHT - settings.MARGIN,
-                                   "Sair", "blue_button")
+        ng_button = buttons.Button(settings.MARGIN, settings.SCREEN_HEIGHT - settings.MARGIN, "Novo jogo",
+                                   "blue_button")
+        controls_button = buttons.Button(settings.MARGIN + settings.BT_DIST, settings.SCREEN_HEIGHT - settings.MARGIN,
+                                         "Controles", "blue_button")
+        settings_button = buttons.Button(settings.MARGIN + 2 * settings.BT_DIST,
+                                         settings.SCREEN_HEIGHT - settings.MARGIN,
+                                         "Opções", "blue_button")
+        quit_button = buttons.Button(settings.MARGIN + 3 * settings.BT_DIST, settings.SCREEN_HEIGHT - settings.MARGIN,
+                                     "Sair", "blue_button")
 
         ng_button.draw_button(self.scn)
         controls_button.draw_button(self.scn)
@@ -94,20 +92,19 @@ class TitleScreen(Screen):
                 running, state = setting_screen.run(running)
         if quit_button.rectangle.collidepoint((mx, my)):
             if click:
-                running = False
                 pygame.quit()
                 sys.exit()
 
         return running
 
-class SettingsScreen(Screen):
 
+class SettingsScreen(Screen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.state = ""
         Screen.set_bg(self, "bg_title_screen.png")
 
-    def run_events(self, running, state = "Default"):
+    def run_events(self, running, state="Default"):
         mx, my = pygame.mouse.get_pos()
         back_button = buttons.Button(settings.MARGIN + 3 * settings.BT_DIST,
                                      settings.SCREEN_HEIGHT - settings.MARGIN,
@@ -128,21 +125,21 @@ class SettingsScreen(Screen):
         music_setting_button = buttons.Button(settings.MARGIN + settings.BT_DIST,
                                               settings.MARGIN + settings.BT_HEIGHT,
                                               "Música", "blue_button")
-        music_setting_button_infantaria = buttons.Button(settings.MARGIN ,
-                                                 settings.MARGIN + settings.BT_HEIGHT,
-                                                 "Soldado", "blue_button")
+        music_setting_button_infantaria = buttons.Button(settings.MARGIN,
+                                                         settings.MARGIN + settings.BT_HEIGHT,
+                                                         "Soldado", "blue_button")
         music_setting_button_avante = buttons.Button(settings.MARGIN + 2 * settings.BT_DIST,
-                                                  settings.MARGIN + settings.BT_HEIGHT,
-                                                  "Avante", "blue_button")
+                                                     settings.MARGIN + settings.BT_HEIGHT,
+                                                     "Avante", "blue_button")
         music_setting_button_infantaria.draw_button(self.scn)
         music_setting_button.draw_button(self.scn)
         music_setting_button_avante.draw_button(self.scn)
         dificuldade_setting_button = buttons.Button(settings.MARGIN + settings.BT_DIST,
                                                     settings.MARGIN + 3 * settings.BT_HEIGHT,
                                                     "Nível", "blue_button")
-        dificuldade_setting_button_easy = buttons.Button(settings.MARGIN ,
-                                                       settings.MARGIN + 3 * settings.BT_HEIGHT,
-                                                       "Facil", "blue_button")
+        dificuldade_setting_button_easy = buttons.Button(settings.MARGIN,
+                                                         settings.MARGIN + 3 * settings.BT_HEIGHT,
+                                                         "Facil", "blue_button")
         dificuldade_setting_button_off = buttons.Button(settings.MARGIN + 2 * settings.BT_DIST,
                                                         settings.MARGIN + 3 * settings.BT_HEIGHT,
                                                         "Difícil", "blue_button")
@@ -200,11 +197,13 @@ class SettingsScreen(Screen):
             pygame.display.update()
         return running, self.state
 
-    def clickstate(self,button):
+    def clickstate(self, button):
         button.button_img = pygame.transform.scale(
             pygame.image.load(f'img/buttons/green_button.png'),
             (settings.BT_WIDTH, settings.BT_HEIGHT))
         button.draw_button(self.scn)
+
+
 class ControlsScreen(Screen):
 
     def __init__(self, *args, **kwargs):
@@ -214,15 +213,15 @@ class ControlsScreen(Screen):
     def run_events(self, running):
         mx, my = pygame.mouse.get_pos()
 
-        back_button = quit_button = buttons.Button(settings.MARGIN + 3*settings.BT_DIST,
-                                   settings.SCREEN_HEIGHT - settings.MARGIN,
-                                   "Voltar", "blue_button")
+        back_button = buttons.Button(settings.MARGIN + 3 * settings.BT_DIST,
+                                     settings.SCREEN_HEIGHT - settings.MARGIN,
+                                     "Voltar", "blue_button")
 
         back_button.draw_button(self.scn)
 
-        #font = pygame.font.Font("fonts/chalkduster.ttf", 60)
-        #text = font.render("Desenvolvimento futuro.", True, (29, 13, 64))
-        #self.scn.blit(text, (50, 80))
+        # font = pygame.font.Font("fonts/chalkduster.ttf", 60)
+        # text = font.render("Desenvolvimento futuro.", True, (29, 13, 64))
+        # self.scn.blit(text, (50, 80))
 
         click = False
         for event in pygame.event.get():
@@ -238,6 +237,7 @@ class ControlsScreen(Screen):
                 running = title_screen.run(running)
 
         return running
+
 
 class GameScreen(Screen):
     def __init__(self, *args, **kwargs):
@@ -264,9 +264,8 @@ class GameScreen(Screen):
         line = 0
         skip = False
 
-        for intro in full_intro_list :
+        for intro in full_intro_list:
             for i in range(len(intro)):
-
                 # verificando se nao ha skip
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
@@ -283,7 +282,7 @@ class GameScreen(Screen):
                     for intro_line in full_intro_list:
                         text_surface = smallfont.render(intro_line, True, WHITE)
                         text_rect = text_surface.get_rect()
-                        text_rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2 + (line-1) * linespace)
+                        text_rect.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + (line - 1) * linespace)
                         line += 1
                         s = (text_surface, text_rect)
                         surfaces_list.append(s)
@@ -295,7 +294,7 @@ class GameScreen(Screen):
                 text += intro[i]
                 text_surface = smallfont.render(text, True, WHITE)
                 text_rect = text_surface.get_rect()
-                text_rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2 -linespace + line*linespace)
+                text_rect.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - linespace + line * linespace)
                 self.scn.blit(text_surface, text_rect)
                 pygame.display.update()
                 pygame.time.wait(60)
@@ -304,7 +303,7 @@ class GameScreen(Screen):
                 continue
             else:
                 s = (text_surface, text_rect)
-                line+=1
+                line += 1
                 surfaces_list.append(s)
                 text = ''
 
@@ -344,14 +343,14 @@ class GameScreen(Screen):
             if skip:
                 text_surface = smallfont.render(password_text, True, WHITE)
                 text_rect = text_surface.get_rect()
-                text_rect.center = (SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+                text_rect.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
                 continue
 
             self.scn.fill(BLACK)
             text += password_text[i]
             text_surface = smallfont.render(text, True, WHITE)
             text_rect = text_surface.get_rect()
-            text_rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+            text_rect.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
             self.scn.blit(text_surface, text_rect)
             pygame.display.update()
             pygame.time.wait(60)
@@ -370,9 +369,9 @@ class GameScreen(Screen):
 
 
 def pop_up(player, enemy, screen, qa, itens_icon):
-    tempo = pygame.time.Clock();
+    tempo = pygame.time.Clock()
     ms = 1000
-    time_lim = 10*ms
+    time_lim = 10 * ms
     answered, correct = False, False
     shield = False
     question, ans = qa.get_qa(enemy.level)
@@ -413,7 +412,7 @@ def pop_up(player, enemy, screen, qa, itens_icon):
 
         # Enemy Life
         pl_life = font.render(str(enemy.life), True, RED)
-        vida = pl_life.get_rect(center=(1100,7.75 * TILESIZE))
+        vida = pl_life.get_rect(center=(1100, 7.75 * TILESIZE))
         screen.blit(pl_life, vida)
 
         # Rect for actions
@@ -421,30 +420,25 @@ def pop_up(player, enemy, screen, qa, itens_icon):
         rect.midbottom = (SCREEN_WIDTH / 2, SCREEN_HEIGHT - TILESIZE / 4)
 
         # Buttons
-        position_x = (SCREEN_WIDTH - BT_WIDTH)/2
-        position_y = rect.y + rect.h/2 - TILESIZE/8
-       # print(sample, ans)
+        position_x = (SCREEN_WIDTH - BT_WIDTH) / 2
+        position_y = rect.y + rect.h / 2 - TILESIZE / 8
 
-        atck2 = buttons.ButtonFight(position_x,position_y
-                                    ,
-                                    ans[sample[0]], "withe_button", sample[0])
+        atck2 = buttons.ButtonFight(position_x, position_y, ans[sample[0]], "withe_button", sample[0])
 
         space = TILESIZE
-        atck1 = buttons.ButtonFight(position_x - atck2.rectangle.w - space,
-                                   position_y,
-                                   ans[sample[1]], "withe_button", sample[1])
+        atck1 = buttons.ButtonFight(position_x - atck2.rectangle.w - space, position_y,
+                                    ans[sample[1]], "withe_button", sample[1])
 
-        atck3 = buttons.ButtonFight(position_x + atck2.rectangle.w + space,
-                                   position_y,
-                                   ans[sample[2]], "withe_button", sample[2])
+        atck3 = buttons.ButtonFight(position_x + atck2.rectangle.w + space, position_y,
+                                    ans[sample[2]], "withe_button", sample[2])
 
         atck1.draw_button(screen)
         atck2.draw_button(screen)
         atck3.draw_button(screen)
 
         pos_center = []
-        for i in range(0,5):
-            position = (700 + 108*i, rect_player.midleft[1])
+        for i in range(0, 5):
+            position = (700 + 108 * i, rect_player.midleft[1])
             pos_center.append(position)
 
         for i in range(0, 5):
@@ -476,22 +470,22 @@ def pop_up(player, enemy, screen, qa, itens_icon):
                 player.itens[0] -= 1
             if itens_icon[1].rectangle.collidepoint((mx, my)) and player.itens[1] > 0:
                 player.life += 30
-                player.itens[1]-= 1
+                player.itens[1] -= 1
             if itens_icon[2].rectangle.collidepoint((mx, my)) and player.itens[2] > 0:
                 enemy.life -= 50
                 answered, correct = True, True
-                player.itens[2]-= 1
+                player.itens[2] -= 1
             if itens_icon[3].rectangle.collidepoint((mx, my)) and player.itens[3] > 0:
                 enemy.life -= 10000
                 answered, correct = True, True
-                player.itens[3]-= 1
+                player.itens[3] -= 1
             if itens_icon[4].rectangle.collidepoint((mx, my)) and player.itens[4] > 0:
                 shield = True
-                player.itens[4]-= 1
+                player.itens[4] -= 1
 
-        text_time = round(time_lim/1000)
+        text_time = round(time_lim / 1000)
         tempo_text = font.render(str(text_time), True, WHITE)
-        tempo_title= tempo_text.get_rect(center=(SCREEN_WIDTH / 2, TILESIZE*1.5 + TILESIZE/4))
+        tempo_title = tempo_text.get_rect(center=(SCREEN_WIDTH / 2, TILESIZE * 1.5 + TILESIZE / 4))
         screen.blit(tempo_text, tempo_title)
         time_lim -= tempo.tick()
 
@@ -499,20 +493,20 @@ def pop_up(player, enemy, screen, qa, itens_icon):
 
         # Question Text
 
-        qSurface = pygame.Surface((TILESIZE*10, TILESIZE*4))  # the size of your rect
-        qSurface.set_alpha(128)  # alpha level
-        qSurface.fill(WHITE)  # this fills the entire surface
-        rect = qSurface.get_rect(midtop = (tempo_title.midbottom[0],tempo_title.midbottom[1] + TILESIZE/4))
+        q_surface = pygame.Surface((TILESIZE * 10, TILESIZE * 4))  # the size of your rect
+        q_surface.set_alpha(128)  # alpha level
+        q_surface.fill(WHITE)  # this fills the entire surface
+        rect = q_surface.get_rect(midtop=(tempo_title.midbottom[0], tempo_title.midbottom[1] + TILESIZE / 4))
 
         question_text = font_text.render(question, True, WHITE)
-        question_rect = question_text.get_rect(center= rect.center)
+        question_rect = question_text.get_rect(center=rect.center)
 
-        screen.blit(qSurface, rect)
+        screen.blit(q_surface, rect)
         screen.blit(question_text, question_rect)
 
         pygame.display.flip()
 
-        if player.life <=0 or enemy.life <=0:
+        if player.life <= 0 or enemy.life <= 0:
             break
 
     if not answered or not correct:
@@ -528,7 +522,7 @@ def gameover(screen):
     font = pygame.font.Font(f'fonts/{BT_FONT}.ttf', 80)
     # Title
     img = font.render('Game Over', True, WHITE)
-    rect = img.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT/2))
+    rect = img.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
     screen.blit(img, rect)
     pygame.display.flip()
 
@@ -614,6 +608,7 @@ def end_animated_text(scn):
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 return
+
 
 def end_participantes(scn):
     global text_surface, text_rect
